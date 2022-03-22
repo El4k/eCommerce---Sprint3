0a79ea5f2dadfa30870bb04ecf1bf72699f179e3
PedroHND <nunespedro03@gmail.com>
2022-03-22 17:12:49 -0300

    Criacao da JPAUtil

---
+++ src/main/java/pear/util/JPAUtil.java
@@ -0,0 +1,15 @@
+package pear.util;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JPAUtil {
+
+	private static final EntityManagerFactory FACTORY = Persistence.createEntityManagerFactory("entregas");
+
+	public static EntityManager getEntityManager() {
+		return FACTORY.createEntityManager();
+
+	}
+}
